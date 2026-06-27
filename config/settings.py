8c7e3007a2ab794ccb70fccd58945c86af9027b9
Your Name <hissainikhalid@gmail.com>
2026-06-27 20:11:52 +0530

    Create modular Django app structure

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+
+    "apps.accounts",
+    "apps.llm",
+    "apps.executor",
+    "apps.history",
+    "apps.api",
 ]
 
 MIDDLEWARE = [
